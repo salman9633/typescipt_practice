54d6e410e53eea37175604ae4d780f659b60cb2a
salmanfaris <salmanvaipees913@gmail.com>
2022-12-11 12:16:08 +0530

    simple practices

---
+++ typescript_w1/test.js
@@ -0,0 +1,6 @@
+"use strict";
+exports.__esModule = true;
+var value = "gdgdfd";
+console.log(value);
+var user = { name: "salman", age: 21 };
+console.log(user.name);
---
+++ typescript_w1/test.ts
@@ -0,0 +1,33 @@
+let value:string = "gdgdfd";
+console.log(value);
+let user={name:"salman",age:21}
+
+console.log(user.name);
+let id=121212
+const cap = "run"
+var len=cap.length
+console.log(id,user,cap,len);
+
+
+//function
+function add(num: number){
+    return num + 5
+}
+
+function stringFn(val : string){
+    return val.toUpperCase
+}
+
+add(2)
+stringFn('saLman')
+
+
+
+
+
+
+
+
+
+
+export{}
