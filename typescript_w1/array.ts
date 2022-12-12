efb3596b6a35ab7e57a87da0a27374b763e50b95
salmanfaris <salmanvaipees913@gmail.com>
2022-12-12 18:42:44 +0530

    array

---
+++ typescript_w1/array.ts
@@ -0,0 +1,37 @@
+const superHeros: string[] = []//declaration
+const power: number[] = []
+
+const powerRank: Array<number> = []//another way of declaring array 
+
+superHeros.push("IronMan")
+power.push(20, 30)
+powerRank.push(1, 2)
+
+/******************************************/
+type User = {
+    name: string
+    isActive: boolean
+}
+
+const allUser: User[]=[]
+
+allUser.push({name:"salman",isActive:true})
+
+/******************************************/
+
+const MLModels: number[][]=[
+    [1,2,3],[9,5,3],[6,0]
+]
+
+console.log(MLModels[1][0]);//op=>9
+console.log(allUser[0].name);//op=>salman
+console.log(powerRank);//op=>[ 1, 2 ]
+console.log(MLModels);//op=>[ [ 1, 2, 3 ], [ 9, 5, 3 ], [ 6, 0 ] ]
+console.log(MLModels[0]);//op=>[ 1, 2, 3 ]
+
+
+
+
+
+
+
--- typescript_w1/objects.ts
+++ typescript_w1/objects.ts
@@ -6,11 +6,11 @@ let user = {
 console.log(user.mail)
 
 //object as params
-// function createUser({ names: string, isPaid: boolean }) { }
+function createUser({ names, isPaid }: { names: string, isPaid: boolean }) { }
 
-// let newUser={ names: "salman", isPaid: true }
+let newUser = { names: "salman", isPaid: true }
 
-// createUser(newUser)
+createUser(newUser)
 
 //returning object
 // function objReturn(): { name: string, price: number } {
@@ -23,10 +23,28 @@ type User = {//setting type
     readonly _id: string;//only read it can't make changes on it
     name: string;
     email: string;
-    active: boolean
+    active: boolean;
+    creditCard?: number//optional
 }
 
 function userDetails(user: User): User {
     return { _id: "", name: "", email: "", active: false }
 }
 userDetails({ _id: "", name: "", email: "", active: false })
+
+
+//compining more types to another
+type cardNumber = {//type 1
+    cardNum: string
+}
+
+type cardDate = {//type 2
+    cardDate: string
+}
+
+type cardDetails = cardNumber & cardDate & { //type 1 and 2 compinig and also new obj too  using '&'
+    cvv: number
+}
+
+
+export { }
